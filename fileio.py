e9fd5ed5eb7fdddc4e2b9b4ba21dd0623ce04391
Saurabh Dhandare <saurabhdhandare@gmail.com>
2024-08-18 20:46:20 +0530

    Add files via upload

---
+++ fileio.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+
+path = Path('pi_digits.txt')
+
+contents = path.read_text()
+contents = contents.strip()
+
+lines = contents.splitlines()
+pi_string = ''
+
+for line in lines:
+    pi_string += line
+
+print(pi_string)
+print(len(pi_string))
